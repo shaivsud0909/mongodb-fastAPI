ff0bcc730d384984a2ac3efa16ec964b7af6bf13
Shaiv Sud <shaiv.sud@tmotions.com>
2026-01-06 12:48:39 +0530

    fast api intigrated

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from schema import UserSchema
+from database import users
+
+app=FastAPI()
+
+@app.get("/read",response_model=list[UserSchema])
+async def read_fun():
+    users_list=[]
+    cursor= users.find()
+    async for user in cursor:
+        users_list.append(user)
+    return users_list
+
+
+@app.put("/add")
+async def create_fun(user:  UserSchema):
+    res=await users.insert_one(user.model_dump())
+    return{        
+    "message": "User created",
+        "id": str(res.inserted_id)
+    }
+
+    
+@app.delete("/delete/{name}")
+async def delete_user(name: str):
+    result = await users.delete_many({"name": name})
+    return{        
+    "message": "User deleted",
+        "deleted": result.deleted_count
+    }
+    
+
+@app.post("/update/{name}")
+async def update_fun(name:str,is_active:bool):
+     result = await users.update_many(
+        {"name": name},
+        {"$set": {"is_active": is_active}}
+     )
+     return {
+        "message": "User updated",
+        "matched": result.matched_count
+     }
